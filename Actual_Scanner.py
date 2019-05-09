25a87204407b546cd3f17e6b7f5496178c4bc8e5
Rahul Gupta <rguptaemail10@gmail.com>
2019-05-09 07:47:05 -0400

    Added instructions, finished program without ino files.

--- Actual_Scanner.py
+++ Actual_Scanner.py
@@ -7,125 +7,210 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#Lowercase Letters
-a = 'a'
-b = 'b'
-c = 'c'
-d = 'd'
-e = 'e'
-f = 'f'
-g = 'g'
-h = 'h'
-i = 'i'
-j = 'j'
-k = 'k'
-l = 'l'
-m = 'm'
-n = 'n'
-o = 'o'
-p = 'p'
-q = 'q'
-r = 'r'
-s = 's'
-t = 't'
-u = 'u'
-v = 'v'
-w = 'w'
-x = 'x'
-y = 'y'
-z = 'z'
-
-#Uppercase Letters
-A = 'A'
-B = 'B'
-C = 'C'
-D = 'D'
-E = 'E'
-F = 'F'
-G = 'G'
-H = 'H'
-I = 'I'
-J = 'J'
-K = 'K'
-L = 'L'
-M = 'M'
-O = 'O'
-P = 'P'
-Q = 'Q'
-R = 'R'
-S = 'S'
-T = 'T'
-U = 'U'
-V = 'V'
-W = 'W'
-X = 'X'
-Y = 'Y'
-Z = 'Z'
-
-#Numbers
-zero = '0'
-one = '1'
-two = '2'
-three = '3'
-four = '4'
-five = '5'
-six = '6'
-seven = '7'
-eight = '8'
-nine = '9'
-
-#Symbols
-at_symbol = '@'
-hashtags = '#'
-us_dollar = '$'
-percentage = '%'
-caret = '^'
-and_symbol = '&'
-asterisk = '*'
-left_parenthesis = '('
-right_parentheis = ')'
-hyphen_minus = '-'
-underscore = '_'
-plus_sign = '+'
-equal_sign = '='
-left_bracket = '['
-right_bracket = ']'
-left_curly_bracket = '{'
-right_curly_bracket = '}'
-vertical_bar = '|'
-slash = '/'
-
-#Punctuation
-period = '.'
-comma = ','
-question_mark = '?'
-exclamtion_point = '!'
 quotation_mark = '"'
-
-#space sign and backspace
 space_sign = ''
 backspace = ''
 
-#Program
+#Character Find Function
+
+def Character_Detection():
+    if 'A' in output:
+        print("A")
+    elif "B" in output:
+        print("B")
+    elif "C" in output:
+        print("C")
+    elif "D" in output:
+        print("D")
+    elif "E" in output:
+        print("E")
+    elif "F" in output:
+        print("F")
+    elif "G" in output:
+        print("G")
+    elif "H" in output:
+        print("H")
+    elif "I" in output:
+        print("I")
+    elif "J" in output:
+        print("J")
+    elif "K" in output:
+        print("K")
+    elif "L" in output:
+        print("L")
+    elif "M" in output:
+        print("M")
+    elif "N" in output:
+        print("N")
+    elif "O" in output:
+        print("O")
+    elif "P" in output:
+        print("P")
+    elif "Q" in output:
+        print("Q")
+    elif "R" in output:
+        print("R")
+    elif "S" in output:
+        print("S")
+    elif "T" in output:
+        print("T")
+    elif "U" in output:
+        print("U")
+    elif "V" in output:
+        print("V")
+    elif "W" in output:
+        print("W")
+    elif "X" in output:
+        print("X")
+    elif "Y" in output:
+        print("Y")
+    elif "Z" in output:
+        print("Z")
+    elif "a" in output:
+        print("a")
+    elif "b" in output:
+        print("b")
+    elif "c" in output:
+        print("c")
+    elif "d" in output:
+        print("d")
+    elif "e" in output:
+        print("e")
+    elif "f" in output:
+        print("f")
+    elif "g" in output:
+        print("g")
+    elif "h" in output:
+        print("h")
+    elif "i" in output:
+        print("i")
+    elif "j" in output:
+        print("j")
+    elif "k" in output:
+        print("k")
+    elif "l" in output:
+        print("l")
+    elif "m" in output:
+        print("m")
+    elif "n" in output:
+        print("n")
+    elif "o" in output:
+        print("o")
+    elif "p" in output:
+        print("p")
+    elif "q" in output:
+        print("q")
+    elif "r" in output:
+        print("r")
+    elif "s" in output:
+        print("s")
+    elif "t" in output:
+        print("t")
+    elif "u" in output:
+        print("u")
+    elif "v" in output:
+        print("v")
+    elif "w" in output:
+        print("w")
+    elif "x" in output:
+        print("x")
+    elif "y" in output:
+        print("y")
+    elif "z" in output:
+        print("z")
+    elif "1" in output:
+        print("1")
+    elif "2" in output:
+        print("2")
+    elif "3" in output:
+        print("3")
+    elif "4" in output:
+        print("4")
+    elif "5" in output:
+        print("5")
+    elif "6" in output:
+        print("6")
+    elif "7" in output:
+        print("7")
+    elif "8" in output:
+        print("8")
+    elif "9" in output:
+        print("9")
+    elif "!" in output:
+        print("!")
+    elif "@" in output:
+        print("@")
+    elif "#" in output:
+        print("#")
+    elif "$" in output:
+        print("$")
+    elif "%" in output:
+        print("%")
+    elif "^" in output:
+        print("^")
+    elif "&" in output:
+        print("&")
+    elif "*" in output:
+        print("*")
+    elif "(" in output:
+        print("(")
+    elif ")" in output:
+        print(")")
+    elif "-" in output:
+        print("-")
+    elif "_" in output:
+        print("_")
+    elif "=" in output:
+        print("=")
+    elif "+" in output:
+        print("+")
+    elif "[" in output:
+        print("[")
+    elif "]" in output:
+        print("]")
+    elif "{" in output:
+        print("{")
+    elif "}" in output:
+        print("}")
+    elif ":" in output:
+        print(":")
+    elif ";" in output:
+        print(";")
+    elif "," in output:
+        print(",")
+    elif "/" in output:
+        print("/")
+    elif "." in output:
+        print(".")
+    elif "<" in output:
+        print("<")
+    elif ">" in output:
+        print(">")
+    elif "?" in output:
+        print("?")
+    elif "'" in output:
+        print(" ' ")
+    elif quotation_mark in output:
+        print("quotation mark")
+    elif space_sign in output:
+        print("space")
+    else:
+        print("nothing")
+
 while True:
-    #time.sleep(10)
-    #im = ImageGrab.grab(bbox=(925,750,2175,835))
+    time.sleep(2)
+    im = ImageGrab.grab(bbox=(925,750,2175,835)) #adjust for your screen
     print("2")
-    #im.save('output.png')
-    #im.show()
+    im.save('output.png')
+    #im.show() #if you want to crash you computer and see the images 
     input = "output.png"
     output = pytesseract.image_to_string(Image.open(input))
-    print(output)
-    result = re.match(a, output)
-        #result_e = re.match(b, output)
-    if result:
-        print("a found successfully")
-        #elif result_e:
-        #    print("e found success")
-        #result = re.match(e, output)
-        #if result:
-        #    print("e found success")
+    text = "Every artist is a man who has freed himself"
+    x = 0
+    while x<43:
+        output = text[x]
+        Character_Detection()
+        x += 1
 
-
-        #elif result:
-            #print("testing")
+if __name__ == '__main__':
+    main()
